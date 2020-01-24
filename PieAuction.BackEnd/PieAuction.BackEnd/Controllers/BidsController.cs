6ebd8e1a3cec649d6bec0f01202adbf39f5fdd57
leighmaj <51490479+leighmaj@users.noreply.github.com>
2020-01-24 16:35:45 -0600

    Added new bid controller. Resolves #3

---
+++ PieAuction.BackEnd/PieAuction.BackEnd/Controllers/BidsController.cs
@@ -0,0 +1,25 @@
+﻿using PieAuction.BackEnd.Data_Access;
+using PieAuction.BackEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PieAuction.BackEnd.Controllers
+{
+    public class BidsController : ApiController
+    {
+		
+	[HttpPost]
+	public Bid NewBid(Bid inBid)
+	{
+		var bidDao = new BidDao();
+		return bidDao.NewBid(inBid);
+	
+	}
+		
+
+	}
+}
